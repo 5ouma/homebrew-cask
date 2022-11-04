648e4abb7d0cad99ce4f4adc3cc26c8968fd30b7
5ouma <101255979+5ouma@users.noreply.github.com>
2022-11-04 19:29:20 +0900

    [add] Shell script to upgrade casks automatically
    By running shell script, `zsh upgradeCask.sh`, it upgrade cask and replace version and sha256 on each ruby file.

---
+++ upgradeCask.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+homeDir="$(dirname $0)"
+caskDir="$homeDir"/Casks
+fileName="$1"
+caskName=$(grep "name" "$caskDir"/"$fileName".rb | sed -e "s/.*name //" -e "s/\"//g")
+if [[ -z "$fileName" ]]; then
+  echo "What app do you want to upgrade?"
+  read -r fileName
+fi
+  if [[ -z $(find "$caskDir" -name "$fileName.*") ]]; then
+    echo -e "\033[31mError:\033[m No available cask with the name \"$fileName\""
+    return 1
+  fi
+ver="$2"
+if [[ -z "$ver" ]]; then
+  echo "Which version do you want to upgrade to?"
+  read -r ver
+fi
+
+sed -i "" "s/version \".*\"/version \"$ver\"/" "$caskDir"/"$fileName".rb
+sed -i "" "s/sha256 \".*\"/sha256 :no_check/" "$caskDir"/"$fileName".rb
+cp "$caskDir"/"$fileName".rb /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask/Casks/
+
+brew upgrade "$fileName"
+hash=$(shasum -a 256 ~/Library/Caches/Homebrew/Cask/"$fileName"--"$ver".* | awk '{print $1}')
+sed -i "" "s/sha256 :no_check/sha256 \"$hash\"/" "$caskDir"/"$fileName".rb
+rm /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask/Casks/"$fileName".rb
+
+git add "$caskDir"/"$fileName".rb
+git commit -m "[upgrade] Version up $caskName to $ver" -m "" -m "Update the cask by updating the app."
+git push origin main
